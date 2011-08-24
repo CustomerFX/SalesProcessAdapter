0d4ff33b86fa918cff1deb1e938a39aa5805aa91
Ryan Farley <ryan@farleyzone.com>
2011-08-24 13:27:09 -0700

    Updated to Sublogix version 1.2

--- FX.SalesProcess/SalesProcessAdapter.cs
+++ FX.SalesProcess/SalesProcessAdapter.cs
@@ -58,10 +58,10 @@ namespace FX.SalesProcess
 			}
 			catch { throw; }
 
-			var salesProcess = Repository.Create<SalesProcesses>();
+			var salesProcess = Repository.Create<Salesprocesses>();
 			salesProcess.EntityId = entityId;
 			salesProcess.Name = ProcessPlugin.Name;
-			salesProcess.BasedOnId = pluginId;
+			salesProcess.BasedonId = pluginId;
 			salesProcess.Isactive = true;
 			salesProcess.Save();
 
@@ -71,16 +71,16 @@ namespace FX.SalesProcess
 			var stages = ProcessPlugin.GetStages();
 			foreach (var stage in stages)
 			{
-				var auditStage = Repository.Create<SalesProcessAudit>();
+				var auditStage = Repository.Create<Salesprocessaudit>();
 				auditStage.EntityId = entityId;
 				auditStage.Name = ProcessPlugin.Name;
 				auditStage.SalesprocessId = salesProcess.SalesprocessesId;
-				auditStage.StageName = stage.Name;
+				auditStage.Stagename = stage.Name;
 				auditStage.Probability = (float)stage.Probability;
 				auditStage.Updateprobability = ProcessPlugin.UpdateOpportunityPercent;
 				auditStage.Processtype = "STAGE";
-				auditStage.StageOrder = stage.OrderNumber;
-				auditStage.StepOrder = 0;
+				auditStage.Stageorder = stage.OrderNumber;
+				auditStage.Steporder = 0;
 				auditStage.Required = false;
 				auditStage.Completed = false;
 				auditStage.StageguidId = stage.Id;
@@ -93,14 +93,14 @@ namespace FX.SalesProcess
 				if (first)
 				{
 					first = false;
-					UpdateOpportunity(entityId, auditStage.Name, auditStage.StageOrder, ProcessPlugin.UpdateOpportunityPercent, auditStage.Probability);
+					UpdateOpportunity(entityId, auditStage.Name, auditStage.Stageorder, ProcessPlugin.UpdateOpportunityPercent, auditStage.Probability);
 
 					auditStage.Startdate = DateTime.Now;
 					auditStage.Startedby = UserId;
-					auditStage.IsCurrent = true;
+					auditStage.Iscurrent = true;
 				}
 				else
-					auditStage.IsCurrent = false;
+					auditStage.Iscurrent = false;
 
 				auditStage.Save();
 
@@ -108,17 +108,17 @@ namespace FX.SalesProcess
 
 				foreach (var step in stage.Steps)
 				{
-					var auditStep = Repository.Create<SalesProcessAudit>();
+					var auditStep = Repository.Create<Salesprocessaudit>();
 					auditStep.EntityId = salesProcess.EntityId;
 					auditStep.Name = ProcessPlugin.Name;
 					auditStep.SalesprocessId = salesProcess.SalesprocessesId;
-					auditStep.StageName = stage.Name;
+					auditStep.Stagename = stage.Name;
 					auditStep.Stepname = step.Name;
 					auditStep.Probability = stage.Probability;
 					auditStep.Updateprobability = ProcessPlugin.UpdateOpportunityPercent;
 					auditStep.Processtype = "STEP";
-					auditStep.StageOrder = stage.OrderNumber;
-					auditStep.StepOrder = step.OrderNumber;
+					auditStep.Stageorder = stage.OrderNumber;
+					auditStep.Steporder = step.OrderNumber;
 					auditStep.Completed = false;
 					auditStep.StageId = auditStage.SalesprocessauditId;
 					auditStep.StageguidId = stage.Id;
@@ -141,17 +141,17 @@ namespace FX.SalesProcess
 			return salesProcess.SalesprocessesId;
 		}
 
-		private void AddProcessElement(SalesProcesses salesProcess, ProcessElementType elementType)
+		private void AddProcessElement(Salesprocesses salesProcess, ProcessElementType elementType)
 		{
-			var audit = Repository.Create<SalesProcessAudit>();
+			var audit = Repository.Create<Salesprocessaudit>();
 			audit.EntityId = salesProcess.EntityId;
 			audit.Name = salesProcess.Name;
 			audit.SalesprocessId = salesProcess.SalesprocessesId;
 			audit.Probability = 0.0F;
 			audit.Updateprobability = ProcessPlugin.UpdateOpportunityPercent;
 			audit.Processtype = elementType.ToString().ToUpper();
-			audit.StageOrder = 0;
-			audit.StepOrder = 0;
+			audit.Stageorder = 0;
+			audit.Steporder = 0;
 			audit.Required = false;
 			audit.Startdate = DateTime.Now;
 			audit.Startedby = UserId;
@@ -162,18 +162,18 @@ namespace FX.SalesProcess
 			audit.Save();
 		}
 
-		private void AddNoStep(SalesProcesses salesProcess, SalesProcessAudit stageAudit)
+		private void AddNoStep(Salesprocesses salesProcess, Salesprocessaudit stageAudit)
 		{
-			var audit = Repository.Create<SalesProcessAudit>();
+			var audit = Repository.Create<Salesprocessaudit>();
 			audit.EntityId = salesProcess.EntityId;
 			audit.Name = salesProcess.Name;
 			audit.SalesprocessId = salesProcess.SalesprocessesId;
-			audit.StageName = stageAudit.Name;
+			audit.Stagename = stageAudit.Name;
 			audit.Stepname = "No Action";
 			audit.Probability = stageAudit.Probability;
 			audit.Processtype = "STEP";
-			audit.StageOrder = stageAudit.StageOrder;
-			audit.StepOrder = 1;
+			audit.Stageorder = stageAudit.Stageorder;
+			audit.Steporder = 1;
 			audit.Completed = false;
 			audit.StageId = stageAudit.SalesprocessauditId;
 			audit.StageguidId = stageAudit.StageguidId;
@@ -183,7 +183,7 @@ namespace FX.SalesProcess
 			audit.Inlan = true;
 			audit.Inweb = true;
 			audit.Description = stageAudit.Name;
-			audit.IsCurrent = false;
+			audit.Iscurrent = false;
 			audit.Seq = SequenceNumber;
 			audit.Data = Encoding.UTF8.GetBytes("<Action></Action>");
 			audit.Save();
